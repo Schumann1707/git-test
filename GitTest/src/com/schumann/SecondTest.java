b32324b23ccce368461860fef496e76c896d8ac5
schumann <schumann@163.com>
2020-02-17 16:26:21 +0800

    开创一个dev分支

---
+++ GitTest/src/com/schumann/SecondTest.java
@@ -0,0 +1,17 @@
+package com.schumann;
+
+import java.sql.SQLOutput;
+
+/**
+ * @description:
+ * @projectName:GitTest
+ * @see:com.schumann
+ * @author:schumann
+ * @createTime:2020-02-17 16:22
+ * @version:1.0
+ */
+public class SecondTest {
+    public static void main(String[] args) {
+        System.out.println("开创一个dev分支");
+    }
+}
