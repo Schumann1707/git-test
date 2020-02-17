b9498e2f7f131751d116f45a1516a6a6665fdc9b
schumann <schumann@163.com>
2020-02-17 15:35:25 +0800

    创建git项目

---
+++ GitTest/src/com/schumann/GitTest.java
@@ -0,0 +1,22 @@
+package com.schumann;
+
+/**
+ * @description:
+ * @projectName:GitTest
+ * @see:com.schumann
+ * @author:schumann
+ * @createTime:2020-02-17 15:11
+ * @version:1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
